853c9ceb2b35e168809dd9337d02a1f36be89770
Brian Wong <bwjing@gmail.com>
2023-12-16 12:00:42 -0800

    prettier

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,28 +1,28 @@
-import Editor from "@/components/Editor";
-import { getUserByClerkId } from "@/utils/auth";
-import { prisma } from "@/utils/db";
+import Editor from '@/components/Editor';
+import { getUserByClerkId } from '@/utils/auth';
+import { prisma } from '@/utils/db';
 
 const getEntry = async (id) => {
-    const user = await getUserByClerkId()
-    const entry = await prisma.journalEntry.findUnique({
-        where: {
-            userId_id: {
-                userId: user.id,
-                id,
-            }
-        }
-    })
+  const user = await getUserByClerkId();
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  });
 
-    return entry;
-}
+  return entry;
+};
 
 const EntryPage = async ({ params }) => {
-    const entry = await getEntry(params.id);
-    return (
-        <div className="w-full h-full">
-            <Editor entry={entry} />
-        </div>
-    )
-}
+  const entry = await getEntry(params.id);
+  return (
+    <div className="w-full h-full">
+      <Editor entry={entry} />
+    </div>
+  );
+};
 
 export default EntryPage;
