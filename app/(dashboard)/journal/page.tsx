33f583c7dc67ef94ff584f1dc625ca516ced1b6c
Brian Wong <bwjing@gmail.com>
2023-12-16 11:32:06 -0800

    update entry page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,6 +2,8 @@ import EntryCard from '@/components/EntryCard';
 import NewEntryCard from '@/components/NewEntryCard';
 import { getUserByClerkId } from '@/utils/auth';
 import { prisma } from '@/utils/db';
+import Link from 'next/link';
+
 const getEntries = async () => {
   const user = await getUserByClerkId();
   const entries = await prisma.journalEntry.findMany({
