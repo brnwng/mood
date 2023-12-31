93450a5efad60c2e8946ed0918476770562ef245
Brian Wong <bwjing@gmail.com>
2023-12-31 13:37:55 -0800

    ask question about entries

--- utils/ai.ts
+++ utils/ai.ts
@@ -2,6 +2,10 @@ import { OpenAI } from 'langchain/llms/openai';
 import { StructuredOutputParser } from 'langchain/output_parsers';
 import { z } from 'zod';
 import { PromptTemplate } from 'langchain/prompts';
+import { Document } from 'langchain/document';
+import { loadQARefineChain } from 'langchain/chains';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -48,4 +52,23 @@ export const analyze = async (prompt) => {
   } catch (e) {
     console.log(e);
   }
+};
+
+export const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, created: entry.created },
+    });
+  });
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = await store.similaritySearch(question);
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  });
+  return res.output_text;
 };
