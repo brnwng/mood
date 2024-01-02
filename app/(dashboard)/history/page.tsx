ba3e87ae09c79c274f83a0207cfce64dfcdb727b
Brian Wong <bwjing@gmail.com>
2024-01-02 15:20:26 -0800

    add sentiment

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,3 +1,18 @@
+import { getUserByClerkId } from '@/utils/auth';
+import { prisma } from '@/utils/db';
+
+const getData = async () => {
+  const user = await getUserByClerkId();
+  const analyses = await prisma.analysis.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: {
+      sentimentScore: true,
+    },
+  });
+};
+
 const History = () => {
   return <div>history</div>;
 };
